d85bcf3d88ce931279f7505fd5b40861a8aa0528
Otto <158194167+Otto529@users.noreply.github.com>
2026-05-13 05:36:36 +0800

    Vite 构建工具配置文件

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // 设置基础路径为相对路径，方便在 GitHub Pages 部署
+})
